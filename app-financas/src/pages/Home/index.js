24653400ba027093c7b792fc67534d3d474d48f8
IsraelAlvesN <nicolauprog21@gmail.com>
2023-05-15 16:49:36 -0300

    Showing list of account movements

--- app-financas/src/pages/Home/index.js
+++ app-financas/src/pages/Home/index.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from '../../components/Header';
+import Balance from '../../components/Balance';
+import Movements from '../../components/Movements';
+
+const list = [
+  {
+    id: 1,
+    label: 'Boleto conta de luz',
+    value: '300,90',
+    date: '17/09/2022',
+    type: 0
+  },
+  {
+    id: 2,
+    label: 'Pix',
+    value: '1.010,00',
+    date: '12/09/2022',
+    type: 1
+  },
+  {
+    id: 3,
+    label: 'Salário',
+    value: '6.300,00',
+    date: '30/09/2022',
+    type: 1
+  },
+]
 
 export default function Home() {
   return (
     <View style={styles.container}>
         <Header name='Primeiro usuário'/>
-        <Text>Page Home</Text>
-        <StatusBar style="auto" />
+        <Balance saldo="9.350,90" gastos="-600,00"/>
+        <Text style={styles.title}>Últimas movimentações</Text>
+
+        <FlatList 
+          style={styles.list} 
+          data={list}
+          keyExtractor={(item) => String(item.id)}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({item}) => <Movements data={item} />}
+        />
     </View>
   );
 }
@@ -15,6 +49,17 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fafafa',
   },
+  title:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight: 18,
+    marginTop: 14
+  },
+  list:{
+    marginStart: 14,
+    marginEnd: 14
+  }
 });
